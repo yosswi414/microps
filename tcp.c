1e7bd2251ca197b89da5e30dbeb83d1b90ec1b04
yosswi414 <yosswi2@gmail.com>
2023-08-30 12:15:52 +0900

    step23 done

--- tcp.c
+++ tcp.c
@@ -5,8 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 
 #include "ip.h"
+#include "platform.h"
 #include "util.h"
 
 #define TCP_FLG_FIN 0x01
@@ -18,6 +20,21 @@
 
 #define TCP_FLG_IS(x, y) ((x & 0x3f) == (y))
 #define TCP_FLG_ISSET(x, y) ((x & 0x3f) & (y) ? 1 : 0)
+
+#define TCP_PCB_SIZE 16
+
+#define TCP_PCB_STATE_FREE 0
+#define TCP_PCB_STATE_CLOSED 1
+#define TCP_PCB_STATE_LISTEN 2
+#define TCP_PCB_STATE_SYN_SENT 3
+#define TCP_PCB_STATE_SYN_RECEIVED 4
+#define TCP_PCB_STATE_ESTABLISHED 5
+#define TCP_PCB_STATE_FIN_WAIT1 6
+#define TCP_PCB_STATE_FIN_WAIT2 7
+#define TCP_PCB_STATE_CLOSING 8
+#define TCP_PCB_STATE_TIME_WAIT 9
+#define TCP_PCB_STATE_CLOSE_WAIT 10
+#define TCP_PCB_STATE_LAST_ACK 11
 
 struct pseudo_hdr {
     uint32_t src;      // Source Address
@@ -38,6 +55,42 @@ struct tcp_hdr {
     uint16_t sum;  // Checksum
     uint16_t up;   // Urgent Pointer
 };
+
+struct tcp_segment_info {
+    uint32_t seq;
+    uint32_t ack;
+    uint16_t len;
+    uint16_t wnd;
+    uint16_t up;
+};
+
+struct tcp_pcb {
+    int state;  // コネクションの状態
+    struct ip_endpoint local;
+    struct ip_endpoint foreign;
+    struct {
+        uint32_t nxt;  // 次に送信するシーケンス番号
+        uint32_t una;  // ACK が返ってきていない最後のシーケンス番号
+        uint16_t wnd;  // 相手の受信ウィンドウ (受信バッファの空き状況)
+        uint16_t up;   // 緊急ポインタ (今回未使用)
+        uint32_t wl1;  // snd.wnd を更新した時の受信セグメントのシーケンス番号
+        uint32_t wl2;  // snd.wnd を更新した時の受信セグメントの ACK 番号
+    } snd;             // 送信時必要な情報
+    uint32_t iss;      // 自分の初期シーケンス番号
+    struct {
+        uint32_t nxt;    // 次に受信を期待するシーケンス番号 (ACK で使われる)
+        uint16_t wnd;    // 自分の受信ウィンドウ (受信バッファの空き状況)
+        uint16_t up;     // 緊急ポインタ (今回未使用)
+    } rcv;               // 受信時必要な情報
+    uint32_t irs;        // 相手の初期シーケンス番号
+    uint16_t mtu;        // 送信デバイスの MTU
+    uint16_t mss;        // 最大セグメントサイズ
+    uint8_t buf[65535];  // receive buffer
+    struct sched_ctx ctx;
+};
+
+static mutex_t mutex = MUTEX_INITIALIZER;
+static struct tcp_pcb pcbs[TCP_PCB_SIZE];
 
 static char* tcp_flg_ntoa(uint8_t flg) {
     static char str[9];  // must be static to keep this ptr not freed
@@ -80,13 +133,165 @@ static void tcp_dump(const uint8_t* data, size_t len, int inout) {
     funlockfile(stderr);
 }
 
+/*
+ *  TCP Protocol Control Block (PCB)
+ *  These should be called after mutex lock
+ */
+static struct tcp_pcb* tcp_pcb_alloc(void) {
+    struct tcp_pcb* pcb;
+
+    // CLOSED で初期化して返す
+    for (pcb = pcbs; pcb < tailof(pcbs); ++pcb) {
+        if (pcb->state == TCP_PCB_STATE_FREE) {
+            pcb->state = TCP_PCB_STATE_CLOSED;
+            sched_ctx_init(&pcb->ctx);
+            return pcb;
+        }
+    }
+    return NULL;
+}
+
+static void tcp_pcb_release(struct tcp_pcb* pcb) {
+    char ep[2][IP_ENDPOINT_STR_LEN];
+
+    // PCB 利用中のタスクがいれば起床させる (解放を他のタスクに任せる)
+    if (sched_ctx_destroy(&pcb->ctx) == -1) {
+        sched_wakeup(&pcb->ctx);
+        return;
+    }
+
+    debugf("released, local=%s, foreign=%s",
+           ip_endpoint_ntop(&pcb->local, ep[0], sizeof(ep[0])),
+           ip_endpoint_ntop(&pcb->foreign, ep[1], sizeof(ep[1])));
+    memset(pcb, 0, sizeof(*pcb));  // pcb->state = TCP_PCB_STATE_FREE (0)
+}
+
+static struct tcp_pcb* tcp_pcb_select(struct ip_endpoint* local, struct ip_endpoint* foreign) {
+    struct tcp_pcb *pcb, *listen_pcb = NULL;
+
+    for (pcb = pcbs; pcb < tailof(pcbs); ++pcb) {
+        // ローカルアドレスがマッチ
+        if ((pcb->local.addr == IP_ADDR_ANY ||
+             pcb->local.addr == local->addr) &&
+            pcb->local.port == local->port) {
+            // ローカルアドレスに bind 可能かどうか調べるとき外部アドレスを指定せずに呼ばれる
+            // ローカルアドレスがマッチしているので、そのまま pcb を返す
+            if (!foreign) return pcb;
+            // 外部アドレスもマッチ
+            if (pcb->foreign.addr == foreign->addr && pcb->foreign.port == foreign->port) return pcb;
+            // 外部アドレスを指定せずに LISTEN しているなら任意の外部アドレスにマッチ (ローカル/外部ともにマッチしたものが優先)
+            if(pcb->state == TCP_PCB_STATE_LISTEN) {
+                // LISTEN with wildcard foreign endpoint
+                if (pcb->foreign.addr == IP_ADDR_ANY && pcb->foreign.port == 0) listen_pcb = pcb;
+            }
+        }
+    }
+    return listen_pcb;
+}
+
+static struct tcp_pcb* tcp_pcb_get(int id) {
+    struct tcp_pcb* pcb;
+
+    if (id < 0 || id >= (int)countof(pcbs)) return NULL;    // out of range
+    pcb = &pcbs[id];
+    return pcb->state == TCP_PCB_STATE_FREE ? NULL : pcb;
+}
+
+static int tcp_pcb_id(struct tcp_pcb* pcb){
+    return indexof(pcbs, pcb);
+}
+
+static ssize_t tcp_output_segment(uint32_t seq, uint32_t ack, uint8_t flg, uint16_t wnd, uint8_t* data, size_t len, struct ip_endpoint* local, struct ip_endpoint* foreign){
+    uint8_t buf[IP_PAYLOAD_SIZE_MAX] = {};
+    struct tcp_hdr* hdr;
+    struct pseudo_hdr pseudo;
+    uint16_t psum;
+    uint16_t total;
+    char ep[2][IP_ENDPOINT_STR_LEN];
+
+    hdr = (struct tcp_hdr*)buf;
+
+    // Exercise 23-1: TCP セグメント生成
+    total = len + sizeof(*hdr);
+    pseudo.src = local->addr;
+    pseudo.dst = foreign->addr;
+    pseudo.zero = 0;
+    pseudo.protocol = IP_PROTOCOL_TCP;
+    pseudo.len = hton16(total);
+    psum = ~cksum16((uint16_t*)&pseudo, sizeof(pseudo), 0);
+
+    hdr->src = local->port;
+    hdr->dst = foreign->port;
+    hdr->seq = hton32(seq);
+    hdr->ack = hton32(ack);
+    hdr->off = (sizeof(*hdr) >> 2) << 4;
+    hdr->flg = flg;
+    hdr->wnd = hton16(wnd);
+    hdr->sum = 0;
+    hdr->up = 0;
+
+    memcpy(hdr + 1, data, len);
+
+    hdr->sum = cksum16((uint16_t*)hdr, total, psum);
+    // Exercise 23-1
+
+    debugf("%s => %s, len=%zu (payload=%zu)",
+           ip_endpoint_ntop(local, ep[0], sizeof(ep[0])),
+           ip_endpoint_ntop(foreign, ep[1], sizeof(ep[1])),
+           total, len);
+    tcp_dump((uint8_t*)hdr, total, -1);
+
+    // Exercise 23-2: IP の送信関数呼び出し
+    if(ip_output(IP_PROTOCOL_TCP, buf, total, local->addr, foreign->addr) == -1){
+        errorf("ip_output() failed");
+        return -1;
+    }
+    // Exercise 23-2
+
+    return len;
+}
+
+static ssize_t tcp_output(struct tcp_pcb* pcb, uint8_t flg, uint8_t* data, size_t len) {
+    uint32_t seq;
+
+    seq = pcb->snd.nxt;
+    // SYN が指定されるのは初回送信時なので初期送信シーケンス番号を使用
+    if (TCP_FLG_ISSET(flg, TCP_FLG_SYN)) seq = pcb->iss;
+    if(TCP_FLG_ISSET(flg, TCP_FLG_SYN | TCP_FLG_FIN) || len) {
+        // TODO: add retransmission queue
+    }
+    return tcp_output_segment(seq, pcb->rcv.nxt, flg, pcb->rcv.wnd, data, len, &pcb->local, &pcb->foreign);
+}
+
+// rfc793 - section 3.9 [Event Processing > SEGMENT ARRIVES]
+static void tcp_segment_arrives(struct tcp_segment_info* seg, uint8_t flags, uint8_t* data, size_t len, struct ip_endpoint* local, struct ip_endpoint* foreign) {
+    struct tcp_pcb* pcb;
+
+    // 使用していないポート宛に届いた TCP セグメントの処理
+    if(!(pcb = tcp_pcb_select(local, foreign)) || pcb->state == TCP_PCB_STATE_CLOSED) {
+        // RST: 無視
+        if (TCP_FLG_ISSET(flags, TCP_FLG_RST)) return;
+        // ACK なし: こちらからは何も送信していないと思われる 相手が送ってきたデータへの ACK 番号 (seg->seq + seg->len) を設定して RST 送信
+        // ACK あり: こちらから何か送信していると思われる (以前に存在していたコネクションのセグメントが遅れて到着など) 相手から伝えられた ACK 番号を SEQ 番号に設定して RST 送信
+        if (!TCP_FLG_ISSET(flags, TCP_FLG_ACK))
+            tcp_output_segment(0, seg->seq + seg->len, TCP_FLG_RST | TCP_FLG_ACK, 0, NULL, 0, local, foreign);
+        else
+            tcp_output_segment(seg->ack, 0, TCP_FLG_RST, 0, NULL, 0, local, foreign);
+        return;
+    }
+    // TODO later
+}
+
 static void tcp_input(const uint8_t* data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface* iface) {
     struct tcp_hdr* hdr;
     struct pseudo_hdr pseudo;
     uint16_t psum;
     char addr[2][IP_ADDR_STR_LEN];
+    struct ip_endpoint local, foreign;
+    uint16_t hlen;
+    struct tcp_segment_info seg;
 
-    if(len < sizeof(*hdr)) {
+    if (len < sizeof(*hdr)) {
         errorf("length %u too small (< TCP header size = %u)", len, sizeof(*hdr));
         return;
     }
@@ -100,14 +305,14 @@ static void tcp_input(const uint8_t* data, size_t len, ip_addr_t src, ip_addr_t 
     pseudo.protocol = IP_PROTOCOL_TCP;
     pseudo.len = hton16(len);
     psum = ~cksum16((uint16_t*)&pseudo, sizeof(pseudo), 0);
-    if(cksum16((uint16_t*)hdr, len, psum)) {
+    if (cksum16((uint16_t*)hdr, len, psum)) {
         errorf("checksum mismatched, sum=0x%04x", cksum16((uint16_t*)data, len, psum));
         return;
     }
     // Exercise 22-3
 
     // Exercise 22-4: アドレスのチェック
-    if(src == IP_ADDR_BROADCAST || dst == IP_ADDR_BROADCAST) {
+    if (src == IP_ADDR_BROADCAST || dst == IP_ADDR_BROADCAST) {
         errorf("neither src nor dst can be broadcast address in TCP, %s => %s",
                ip_addr_ntop(src, addr[0], sizeof(addr[0])),
                ip_addr_ntop(dst, addr[1], sizeof(addr[1])));
@@ -121,6 +326,27 @@ static void tcp_input(const uint8_t* data, size_t len, ip_addr_t src, ip_addr_t 
            len, len - sizeof(*hdr));
 
     tcp_dump(data, len, 1);
+
+    local.addr = dst;
+    local.port = hdr->dst;
+    foreign.addr = src;
+    foreign.port = hdr->src;
+    hlen = (hdr->off >> 4) << 2;
+
+    // tcp_segment_arrives() で必要な情報 (seg.*) を収集
+    seg.seq = ntoh32(hdr->seq);
+    seg.ack = ntoh32(hdr->ack);
+    seg.len = len - hlen;
+    if (TCP_FLG_ISSET(hdr->flg, TCP_FLG_SYN)) ++seg.len;    // SYN consumes one SEQ number
+    if (TCP_FLG_ISSET(hdr->flg, TCP_FLG_FIN)) ++seg.len;    // FIN consumes one SEQ number
+    seg.wnd = ntoh16(hdr->wnd);
+    seg.up = ntoh16(hdr->up);
+    mutex_lock(&mutex);
+    {
+        tcp_segment_arrives(&seg, hdr->flg, (uint8_t*)hdr + hlen, len - hlen, &local, &foreign);
+    }
+    mutex_unlock(&mutex);
+
     return;
 }
 
---
+++ test/step23.c
@@ -0,0 +1,105 @@
+#include <signal.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "driver/ether_tap.h"
+#include "driver/loopback.h"
+#include "icmp.h"
+#include "ip.h"
+#include "net.h"
+#include "test.h"
+#include "udp.h"
+#include "util.h"
+#include "tcp.h"
+
+static volatile sig_atomic_t terminate;
+
+static void on_signal(int s) {
+    (void)s;
+    terminate = 1;
+    net_raise_event();  // イベント発生
+}
+
+static int setup(void) {
+    struct net_device* dev;
+    struct ip_iface* iface;
+
+    // シグナルハンドラの設定 (C-c に対してお行儀よく終了するように)
+    signal(SIGINT, on_signal);
+
+    // プロトコルスタックの初期化
+    if (net_init() == -1) {
+        errorf("net_init() failure");
+        return -1;
+    }
+
+    // ループバックデバイスの初期化
+    // デバイスドライバがプロトコルスタックへの登録まで済ませる
+    dev = loopback_init();
+    if (!dev) {
+        errorf("loopback_init() failure");
+        return -1;
+    }
+    iface = ip_iface_alloc(LOOPBACK_IP_ADDR, LOOPBACK_NETMASK);
+    if (!iface) {
+        errorf("ip_iface_alloc() failed");
+        return -1;
+    }
+    if (ip_iface_register(dev, iface) == -1) {
+        errorf("ip_iface_register() failed");
+        return -1;
+    }
+
+    // Ethernet デバイスの初期化
+    dev = ether_tap_init(ETHER_TAP_NAME, ETHER_TAP_HW_ADDR);
+    if (!dev) {
+        errorf("ether_tap_init() failed");
+        return -1;
+    }
+    iface = ip_iface_alloc(ETHER_TAP_IP_ADDR, ETHER_TAP_NETMASK);
+    if (!iface) {
+        errorf("ip_iface_alloc() failed");
+        return -1;
+    }
+    if (ip_iface_register(dev, iface) == -1) {
+        errorf("ip_iface_register() failed");
+        return -1;
+    }
+    if (ip_route_set_default_gateway(iface, DEFAULT_GATEWAY) == -1) {
+        errorf("ip_route_set_default_gateway() failed");
+        return -1;
+    }
+
+    // プロトコルスタックの起動
+    if (net_run() == -1) {
+        errorf("net_run() failure");
+        return -1;
+    }
+    return 0;
+}
+
+static void cleanup(void) {
+    net_shutdown();
+}
+
+
+#define TRY(f) TRY2(f, do {} while(0))
+
+#define TRY2(f, end)               \
+    do {                          \
+        if ((f) == -1) {          \
+            errorf(#f " failed"); \
+            end;                  \
+            return -1;            \
+        }                         \
+    } while (0)
+
+int main(int argc, char* argv[]) {
+    TRY(setup());
+    while (!terminate) sleep(1);
+    cleanup();
+    return 0;
+}
